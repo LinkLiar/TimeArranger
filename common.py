d5310fe7eeb78018a4513c695ebbac09091d43ea
Link <38213500+LinkLiar@users.noreply.github.com>
2022-04-02 21:58:17 +0800

    Create common.py

---
+++ common.py
@@ -0,0 +1,10 @@
+import datetime
+import time
+
+def NowStr():
+    return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+def CalDeltaSecond(EndStr,StartStr):
+    start = datetime.datetime.strptime(StartStr,"%Y-%m-%d %H:%M:%S")
+    end = datetime.datetime.strptime(EndStr,"%Y-%m-%d %H:%M:%S")
+    return (end - start).total_seconds()
